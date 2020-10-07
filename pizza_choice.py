45552f97f230955a007e9ef22b0eef69acbdbdcf
oponga rolique séverin <categoriederive1.flipflop@gmail.com>
2020-10-07 15:56:00 +0200

    fourme d'ambert

---
+++ pizza_choice.py
@@ -0,0 +1,10 @@
+hungry=input("do you want to eat pizza with fourme d'ambert?")
+if hungry == "yes":
+    print("ok you 'll eat pizza  fourme d'ambert")
+    
+else:
+    thirsty = input("are you thirsty  you'll drink water  !!?")
+    if thirsty=="yes":
+        print("drink water")
+      
+      
