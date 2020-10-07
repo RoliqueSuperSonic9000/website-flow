d3f5a280b10dbb154dc38b0f42d25ec47b4080cf
oponga rolique séverin <categoriederive1.flipflop@gmail.com>
2020-10-07 15:59:37 +0200

    mozzarela

--- pizza_choice.py
+++ pizza_choice.py
@@ -1,6 +1,6 @@
-hungry=input("do you want to eat pizza with  Emental ?")
+hungry=input("do you want to eat pizza with  mozzarela ?")
 if hungry == "yes":
-    print("ok you 'll eat pizza  with Emental")
+    print("ok you 'll eat pizza  with  mozzarela ")
     
 else:
     thirsty = input("are you thirsty  you'll drink water  !!?")
