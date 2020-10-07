450492a1f593c827242366efb87987bf3326aed6
oponga rolique séverin <categoriederive1.flipflop@gmail.com>
2020-10-07 16:00:52 +0200

    chèvre

--- pizza_choice.py
+++ pizza_choice.py
@@ -1,6 +1,6 @@
-hungry=input("do you want to eat pizza with  mozzarela ?")
+hungry=input("do you want to eat pizza with  chèvre ?")
 if hungry == "yes":
-    print("ok you 'll eat pizza  with  mozzarela ")
+    print("ok you 'll eat pizza  with  chèvre ")
     
 else:
     thirsty = input("are you thirsty  you'll drink water  !!?")
